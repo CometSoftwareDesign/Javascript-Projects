add058f1df89995cd77427f6279d628bee042d75
Henry Lee <63754132+CometSoftwareDesign@users.noreply.github.com>
2020-07-19 13:40:49 -0700

    New javascripts

---
+++ Basic_JavaScript_3.js
@@ -0,0 +1,44 @@
+function division(){
+    var simple_Math = 87798 / 46;
+    document.getElementById("Math").innerHTML= " 87798 / 46 = " + simple_Math;
+}
+
+function multiplication() {
+    var simple_Math = 87798 * 46;
+    document.getElementById("Maths").innerHTML = " 87798 X 46 = " + simple_Math;
+}
+
+function more_Math(){ 
+    var simple_Math= (1+2) * 2 / 2 - 3;
+    document.getElementById("math").innerHTML = "1 plus 2, multiplied by 2, divided in half, subtracted by 3 equals" + simple_Math
+}
+
+function modulus_Operator(){
+    var simple_Math= 28 % 6;
+    document.getElementById("maths").innerHTML= "When you divide 28 by 6 you have a remainder of: " +simple_Math 
+}
+
+function negation_Operator(){
+    var x = 20;
+    document.getElementById("Mathyis").innerHTML = -x;
+}
+
+
+
+{
+    var Y = 3.6;
+    Y++;
+    document.write (Y);
+}
+
+//Decriment
+{
+    var G = 600.8;
+    G --;
+    document.write (G);
+}
+//random number in a pop-up
+    window.alert(Math.random() * 100);
+
+    //math objects
+document.getElementById("demo").innerHTML = Math.PI;
