f77ac1630f95da285e8f774ea4bbe6fee63cd64f
Henry Lee <63754132+CometSoftwareDesign@users.noreply.github.com>
2020-07-19 11:15:55 -0700

    JS 2 and HTML2

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+var b="Bienvenue, mon ami! Entres ici"
+window.alert(b)
+
+var A="Allonsy, mon coeur!"
+
+function myFunction() {
+    var song = "Take me over to that other side!";
+    song += "Ooohhhhh";
+    document.getElementById("C").innerHTML = song;
+}
