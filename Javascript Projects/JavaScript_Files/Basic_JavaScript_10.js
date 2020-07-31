10e257a503f61526284042b59893347cdbeeb787
Henry Lee <63754132+CometSoftwareDesign@users.noreply.github.com>
2020-07-30 17:42:56 -0700

    Need Help with const function
    Hi i need assistance on HTML 10 and JS 10 on the Constant Function. My Code isn't working and I can't figure out why.

--- Javascript Projects/JavaScript_Files/Basic_JavaScript_10.js
+++ Javascript Projects/JavaScript_Files/Basic_JavaScript_10.js
@@ -1,7 +1,7 @@
 function Call_Loop(){
    var Number = "";
    var N = 10; 
-   while (N > 2){
+   while (N < 20){
        Number += "<br>" + N;
        N++;
 
@@ -30,4 +30,24 @@ function for_Loop(){
         content += Places[G]+ "<br>";
     }
     document.getElementById ("Ancient_Places").innerHTML = content;
+}
+
+//array function
+
+function array_Function(){
+    var Egypt_Picture = [];
+    Egypt_Picture [0] = " the Pyramids of Giza";
+    Egypt_Picture [1] = "Imhotep's Tomb";
+    Egypt_Picture [2] = " the Eye of Horus";
+document.getElementById ("Array").innerHTML = "This is " + Egypt_Picture [2] + ".";
+}
+
+//End Array Function
+
+//Constant function
+
+function constant_Function() {
+    const Scottish_Empire = {country:"Scotland", capital:"Edinburg"};
+    Scottish_Empire.capital = "Leith";
+    document.getElementById("Constant") .innerhtml = "The Capital of Scotland is " + Scottish_Empire.capital + ".";
 }
